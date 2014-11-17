049937794c7241199fb96e06b5d02101218287d5
Gary Burgess <gary.burgess@gmail.com>
2014-11-17 21:44:29 +0000

    Add grunt build

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    nodeunit: {
+      files: ["test/*.js"],
+    },
+
+    jshint: {
+      options: {
+        jshintrc: ".jshintrc"
+      },
+      gruntfile: {
+        src: "Gruntfile.js"
+      },
+      lib: {
+        src: ["lib/**/*.js"]
+      },
+      test: {
+        src: ["test/**/*.js"]
+      },
+    }
+
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-nodeunit");
+
+  grunt.registerTask("default", ["jshint", "nodeunit"]);
+
+};
